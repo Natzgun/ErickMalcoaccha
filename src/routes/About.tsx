285e67877189b794b72184fed9da57a25ed227e9
Erick <itzlilouxx@gmail.com>
2023-03-17 17:32:25 -0500

    Solved bug haraguchi

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -2,12 +2,14 @@ const About = () => {
   return (
     <div
       data-name="about"
-      className="w-full h-screen bg-[#0a192f] text-gray-300"
+      className="w-full h-screen bg-white text-gray-300"
     >
       <div className="flex flex-col justify-center items-center w-full h-full">
+        <h2 className="text-7xl mb-3 text-slate-500">Haraguchi</h2>
         <img
           src="https://i.ytimg.com/vi/ZL5b4uPicp0/maxresdefault.jpg"
           alt=""
+          className="h-[400px]"
         />
       </div>
     </div>
